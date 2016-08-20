6884a7140b8a65502ef6c72a1affcd56b79f4aaa
MindSparkTm <smartsurajit2008@gmail.com>
2016-08-20 16:07:33 +0300

    Update MainClass.java

--- ImprovementonDuplicatesinanArray/src/MainClass.java
+++ ImprovementonDuplicatesinanArray/src/MainClass.java
@@ -8,18 +8,10 @@ public class MainClass {
 		// TODO Auto-generated method stub
 	      HashSet<Integer> hs = new HashSet<Integer>();
 	       int [] a= {10,7,100,7,9,9,10,10,11,11,1};
-	       Boolean flag = false;
+	       
 	       for(int i=0;i<a.length;i++)
 	       {
-	    	   if(hs.contains(a[i]))
-	    	   {
-	    		   flag = false;
-	    	   }
-	    	   
-	    	   else{
-	    		   flag = true;
-	    		   hs.add(a[i]);
-	    	   }
+	    	   hs.add(a[i]);
 	       }
 	       
 	       Integer [] rtrn = new Integer[hs.size()];
